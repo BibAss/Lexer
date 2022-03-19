16ca046be7392f983bd58237df0e12efcf1c8d1b
BibAss <byba003@mail.ru>
2022-03-19 23:35:05 +0300

    done lexer for 1 str without "space"

--- main/src/Lexer.java
+++ main/src/Lexer.java
@@ -3,23 +3,67 @@ import java.util.List;
 import java.util.regex.Matcher;
 
 
-public class Lexer {
+public class Lexer
+{
     private final String code;
     List<Token> tokens = new LinkedList<>();
 
-    Lexer(String code){
+    Lexer(String code)
+    {
         this.code = code;
     }
 
-    public void Analise(){
-        for (String lexemName: Regexp.lexems.keySet()) {
-            Matcher m = Regexp.lexems.get(lexemName).matcher(code);
-            while (m.find()) {
-                System.out.println(lexemName + " found ");
-                tokens.add(new Token(lexemName, m.group()));
+    public void Analise()
+    {
+        int staticIndex = 0, dynamicIndex = 0;
+        int crutch = 0;
+        boolean stop = false;
+        while(true)
+        {
+            dynamicIndex += 1;
+            if(stop)
+            {
+                break;
             }
+            if(dynamicIndex > code.length())
+            {
+                dynamicIndex -= 1;
+                stop = true;
+            }
+            StringBuilder buffer = new StringBuilder(code.substring(staticIndex, dynamicIndex));
+            if(stop)
+            {
+                buffer.append(" ");
+            }
+            for (String lexemeName: Regexp.lexems.keySet())
+            {
+                Matcher matcher = Regexp.lexems.get(lexemeName).matcher(buffer);
+                if(matcher.matches())
+                {
+                    crutch += 1;
+                }
+            }
+            if(crutch == 0)
+            {
+                buffer.delete(buffer.length() - 1, buffer.length());
+                String token = buffer.toString();
+                for (String lexemeName: Regexp.lexems.keySet())
+                {
+                    Matcher matcher = Regexp.lexems.get(lexemeName).matcher(buffer);
+                    if(matcher.matches())
+                    {
+                        tokens.add(new Token(lexemeName, token));
+                        break;
+                    }
+                }
+                staticIndex = dynamicIndex - 1;
+            }
+            crutch = 0;
         }
+    }
 
+    public void Tokens()
+    {
         for (Token token: tokens) {
             System.out.println(token);
         }
--- main/src/main.java
+++ main/src/main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        Lexer lex = new Lexer("if (mama = (3 + 4))");
+        Lexer lex = new Lexer("papa=((a+b+c+d+var*c))if(n=m)m"); // mama=(3+4)
         lex.Analise();
+        lex.Tokens();
     }
 }
